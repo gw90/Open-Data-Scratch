ba93c7ef0feb208763607c58961a7010d07fbb9b
Thepuzzlegame <sethmiller464@yahoo.com>
2015-06-07 20:49:12 -0700

    changed data storage method

---
+++ list.js
@@ -0,0 +1,338 @@
+var extensions = [
+    {
+        "title": "Alarm extension",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/alarm_extension.js#scratch",
+        "descriptions": "Adds alarm countdown system",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Ambient Light",
+        "author": "djdolphin",
+        "links": "http://scratchx.org/?url=http://dylan.cf/scratchx/ambientlight.js#scratch",
+        "descriptions": "Gets reading on ambient light",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Battery Status",
+        "author": "technoboy10",
+        "links": "http://scratchx.org/?url=http://technoboy10.tk/scratchx/battery.js#scratch",
+        "descriptions": "Reports on current battery status",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Be Cool",
+        "author": "djdolphin, bobbybee",
+        "links": "http://scratchx.org/?url=http://dylan.cf/scratchx/becool.js#scratch",
+        "descriptions": "Editor rotation!",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Browser Info and Language",
+        "author": "andrewjcole",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Browser_Info_and_Language.js#scratch",
+        "descriptions": "Gets browser info such as language",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Browser Stuff",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/browser_extension.js#scratch",
+        "descriptions": "Open JS dialog boxes and new tabs",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Cloud Data",
+        "author": "technoboy10",
+        "links": "http://scratchx.org/?url=http://technoboy10.tk/scratchx/cloud.js#scratch",
+        "descriptions": "Grabs and reads cloud data from projects",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Cookie",
+        "author": "gregory9",
+        "links": "http://scratchx.org/?url=http://gw90.github.io/Complete-Extensions/cookie-extension.js#scratch, http://gw90.github.io/scratch-extension/",
+        "descriptions": "This extension allows you to use cookies with Scratch",
+        "linktitles": "ScratchX, Doc"
+    },
+    {
+        "title": "Cookie Variables",
+        "author": "djdolphin",
+        "links": "http://scratchx.org/?url=http://dylan.cf/scratchx/cookievariables.js#scratch",
+        "descriptions": "Enables cookie variables in the editor",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Cool helpful things extension",
+        "author": "PullJosh",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Cool_helpful_things_extension.js#scratch",
+        "descriptions": "Math blocks/other features",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Data",
+        "author": "Thepuzzlegame",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Data.js#scratch",
+        "descriptions": "Adds data manipulating blocks and string operations",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Debugging",
+        "author": "savaka",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Debugging.js#scratch",
+        "descriptions": "Adds inline comments and a block to print to the console",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Extension Loader",
+        "author": "Thepuzzlegame",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Extension_Loader.js#scratch",
+        "descriptions": "Loads extensions from URLs",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Extension Status Getter",
+        "author": "Znapi",
+        "links": "http://scratchx.org/?url=http://znapi.github.io/scratchx/extension-status-getter/main.min.js#scratch, https://github.com/Znapi/scratchx/wiki/Extension-Status-Getter",
+        "descriptions": "Gets the status code and message of any loaded extension",
+        "linktitles": "ScratchX, Doc"
+    },
+    {
+        "title": "FeedMe RSS fun",
+        "author": "powerpoint56",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/FeedMe_RSS_fun.js#scratch",
+        "descriptions": "Adds functionality from the Google Feed API",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Gamepad",
+        "author": "bobbybee",
+        "links": "http://scratchx.org/?url=https://rawgit.com/bobbybee/scratch-gamepad/master/ext.js#scratch",
+        "descriptions": "A thin wrapper around the Gamepad API which supports Xbox 360 controllers, most PS3 controllers, and other non-brand D-Pad controllers",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Go to Project",
+        "author": "MegaApuTurkUltra",
+        "links": "http://scratchx.org/?url=https://rawgit.com/MegaApuTurkUltra/Scratch-Go-To-Project/master/go-to-project.ext.js#scratch",
+        "descriptions": "Goes to projects",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Hash",
+        "author": "savaka",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Hash.js#scratch",
+        "descriptions": "Performs hashing algorithms",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "HQ Sound",
+        "author": "MegaApuTurkUltra",
+        "links": "http://scratchx.org/?url=https://rawgit.com/MegaApuTurkUltra/Scratch-HQ-Sound/master/scratch-hqs.ext.js#scratch",
+        "descriptions": "Allows for high quality sound manipulation within Scratch",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Infinity Reporter",
+        "author": "Thepuzzlegame",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Infinity_Reporter.js#scratch",
+        "descriptions": "Adds an infinity reporter",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "JavaScript Dialogs",
+        "author": "savaka",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Javascript_Dialogs.js#scratch",
+        "descriptions": "Opens JS dialog boxes",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Joystick",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/joystickExtension.js#scratch",
+        "descriptions": "Gets values of a joystick",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Link Opener",
+        "author": "savaka",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Link_Opener.js#scratch",
+        "descriptions": "Goes to websites",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "littleBits",
+        "author": "khanning",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/littleBits.js#scratch, https://github.com/khanning/scratch-littlebits-extension",
+        "descriptions": "Connects to littleBits",
+        "linktitles": "ScratchX, Doc"
+    },
+    {
+        "title": "Local Storage",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/localstorage_extension.js#scratch",
+        "descriptions": "Uses JavaScript localStorage API",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Logic",
+        "author": "savaka",
+        "links": "http://scratchx.org/?url=http://a.pomf.se/fdzopv.js#scratch",
+        "descriptions": "Various logic gates",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Math",
+        "author": "gregory9",
+        "links": "http://scratchx.org/?url=http://gw90.github.io/Complete-Extensions/math-extension.js#scratch, http://gw90.github.io/scratch-extension/",
+        "descriptions": "This extension has a few blocks for math",
+        "linktitles": "ScratchX, Doc"
+    },
+    {
+        "title": "MathExt",
+        "author": "StarStudios23",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/MathExt.js#scratch",
+        "descriptions": "Multiple operators and a useful block for getting the hypotenuse of a triangle",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Mesh",
+        "author": "bobbybee",
+        "links": "http://scratchx.org/?url=https://rawgit.com/bobbybee/mesh-2.0/master/ext.js#scratch",
+        "descriptions": "Enables mesh features",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Minecraft Server Checker",
+        "author": "Mrcomputer1",
+        "links": "http://scratchx.org/?url=http://mrcomputer1.github.io/MinecraftServerCheckerExtension/MinecraftExtension.js#scratch, http://mrcomputer1.github.io/MinecraftServerCheckerExtension/",
+        "descriptions": "Gets data from minecraft servers",
+        "linktitles": "ScratchX, Doc"
+    },
+    {
+        "title": "More Math",
+        "author": "savaka",
+        "links": "http://scratchx.org/?url=http://a.pomf.se/yfqifs.js#scratch",
+        "descriptions": "Adds a lot more math stuff",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "More Operators",
+        "author": "savaka",
+        "links": "http://scratchx.org/?url=http://a.pomf.se/ruvypv.js#scratch",
+        "descriptions": "Additional operators for Scratch",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "PhysExt",
+        "author": "StarStudios23",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/PhysExt.js#scratch",
+        "descriptions": "Gets bounce ranges for objects",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "PicoBoard",
+        "author": "SCRAXE",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/scraxePicoExtension.js#scratch",
+        "descriptions": "Connect to PicoBoard with additional features",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "PicoBoard",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/picoExtension.js#scratch",
+        "descriptions": "Connects to PicoBoard",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Power Extension",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/power_extension.js#scratch",
+        "descriptions": "Adds a power block",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Random wait Extension",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/random_wait_extension.js#scratch",
+        "descriptions": "Adds random wait block",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Speech to Text",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/speech_to_text_extension.js#scratch",
+        "descriptions": "Uses JavaScript Speech API for speech to text",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Splendid Strings",
+        "author": "BookOwl",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Splendid_Strings.js#scratch",
+        "descriptions": "Adds blocks for working with strings",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "StringExt",
+        "author": "StarStudios23",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/StringExt.js#scratch",
+        "descriptions": "Several string manipulating features",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Temp Vars and Lists",
+        "author": "Zro716",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/Temp_Vars_and_Lists.js#scratch",
+        "descriptions": "Temporary variables and lists",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Text to Speech",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/text_to_speech_extension.js#scratch",
+        "descriptions": "Uses JavaScript Speech API for text to speech",
+        "linktitles": "ScratchX"
+    },
+	{
+        "title": "Text to Speech Simple",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/text_to_speech_simple_extension.js#scratch",
+        "descriptions": "Simple text to speech extension",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "VarExt",
+        "author": "StarStudios23",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/VarExt.js#scratch",
+        "descriptions": "A couple blocks for working with variables",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Weather Extension",
+        "author": "Scratchteam",
+        "links": "http://scratchx.org/?url=https://rawgit.com/sayamindu/scratch-extensions/master/weather_extension.js#scratch",
+        "descriptions": "Reports temperature for city",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "WebExt",
+        "author": "StarStudios23",
+        "links": "http://scratchx.org/?url=http://thepuzzlegame.github.io/extensions/WebExt.js#scratch",
+        "descriptions": "Receives page data and opens urls",
+        "linktitles": "ScratchX"
+    },
+    {
+        "title": "Xi4s English",
+        "author": "itchy20",
+        "links": "http://scratchx.org/?url=http://MrYsLab.github.io/xi4s.js#scratch, https://github.com/MrYsLab/xi",
+        "descriptions": "Simultaneously connect, monitor and control multiple Arduino, BeagleBone Black and Raspberry Pi boards",
+        "linktitles": "ScratchX, Doc"
+    },
+    {
+        "title": "Xi4s French",
+        "author": "itchy20",
+        "links": "http://scratchx.org/?url=http://MrYsLab.github.io/xi4s_fr4.js#scratch, https://github.com/MrYsLab/xi",
+        "descriptions": "Xi4s in french",
+        "linktitles": "ScratchX, Doc"
+    }
+];
